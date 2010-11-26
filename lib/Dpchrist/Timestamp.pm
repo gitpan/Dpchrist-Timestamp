ef8beab841e7b96aa75514608abbd844683d44b0
David Paul Christensen <dpchrist@holgerdanske.com>
2010-11-26 21:29:54 +0000

    Import of DPCHRIST/Dpchrist-Timestamp-1.014 from CPAN.
    gitpan-cpan-distribution: Dpchrist-Timestamp
    gitpan-cpan-version:      1.014
    gitpan-cpan-path:         DPCHRIST/Dpchrist-Timestamp-1.014.tar.gz
    gitpan-cpan-author:       DPCHRIST
    gitpan-cpan-maturity:     released

--- lib/Dpchrist/Timestamp.pm
+++ lib/Dpchrist/Timestamp.pm
@@ -1,5 +1,5 @@
 #######################################################################
-# $Id: Timestamp.pm,v 1.13 2010-11-25 18:24:56 dpchrist Exp $
+# $Id: Timestamp.pm,v 1.14 2010-11-26 20:34:45 dpchrist Exp $
 #######################################################################
 # package stuff:
 #----------------------------------------------------------------------
@@ -22,7 +22,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = ();
 
-our $VERSION = sprintf "%d.%03d", q$Revision: 1.13 $ =~ /: (\d+)\.(\d+)/;
+our $VERSION = sprintf "%d.%03d", q$Revision: 1.14 $ =~ /: (\d+)\.(\d+)/;
 
 #######################################################################
 # functions:
@@ -67,7 +67,7 @@ Dpchrist::Timestamp - generate time stamps
 
 =head1 DESCRIPTION
 
-This documentation describes module revision $Revision: 1.13 $.
+This documentation describes module revision $Revision: 1.14 $.
 
 
 This is alpha test level software
